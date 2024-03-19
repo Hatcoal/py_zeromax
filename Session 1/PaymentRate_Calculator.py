caef712e124e3045e3e24354a706d0fae69785b7
Hatcoal <157669460+Hatcoal@users.noreply.github.com>
2024-03-19 16:59:23 +0700

    Payment

---
+++ Session 1/PaymentRate_Calculator.py
@@ -0,0 +1,4 @@
+num_hours= int(input("Enter the number of hours worked:"))
+paymentrate = float(input("Enter the payment per hour:"))
+total_payment = num_hours * paymentrate
+print("Total payment:", total_payment)
